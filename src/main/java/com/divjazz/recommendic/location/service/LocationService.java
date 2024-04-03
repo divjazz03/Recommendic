fa748c759d8a91443ca10645606cedb6ca13cb5d
divjazz03 <divjazz03@gmail.com>
2024-04-03 10:20:19 +0100

    Minor change in the Location Service

--- src/main/java/com/divjazz/recommendic/location/service/LocationService.java
+++ src/main/java/com/divjazz/recommendic/location/service/LocationService.java
@@ -1,5 +1,14 @@
 package com.divjazz.recommendic.location.service;
 
+import com.google.maps.GeoApiContext;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LocationService {
 
+    private final GeoApiContext geoApiContext;
+
+    public LocationService(GeoApiContext geoApiContext) {
+        this.geoApiContext = geoApiContext;
+    }
 }
